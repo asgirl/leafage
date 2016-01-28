479f7ae45b8aac7c006e82acf5348f4bc5a75544
A.S <maynqna@ya.ru>
2016-01-28 21:59:39 +0500

    settings

---
+++ #manager/Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          hostname: 'localhost',
+          port: 8080,
+          base: '../',
+          keepalive: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['connect']);
+};
